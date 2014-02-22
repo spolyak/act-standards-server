6865a220e3aee5b144c2b3fa3a0a4ccd44eac125
Steve Polyak <stevepolyak@mac.com>
2014-02-22 12:49:26 -0600

    gather JSON children for standards doc

--- handlers/data.js
+++ handlers/data.js
@@ -4,42 +4,39 @@ var StandardsModel = sm.StandardsModel;
 
 var doc = {};
 var results = [];
-var tArr = [];
 
 exports.json = function(req, res) {
-
-	StandardsModel.findOne({
-		uri : new RegExp(req.params.id + '$', "")
-	}, function(err, sd) {
-		if (!err) {
-
-			var children = sd.children;
-			sd.children = [];
-			var foo = [];
-			if(children) {
-				  async.forEach(children, processEachChild, afterAllChildren);
-
-				  function processEachChild(child, callback) {
-				    console.log(child);
-				    StandardsModel.findOne({'_id': child}, function(err,res) {
-				      tArr.push(res); 
-				      //console.log(res);
-				      callback(err);
-				    });
-				  }
-
-				  function afterAllChildren(err) {
-				    console.log("woo " +tArr);
-				    sd.children = tArr;
-				    res.send(sd);
-				  }
-			}
-		} else {
-			if ('CastError' === err.name && 'ObjectId' === err.type) {
-				return res.status(404).send();
+	StandardsModel.find({}, function(err, docs) {
+		StandardsModel.findOne({
+			uri : new RegExp(req.params.id + '$', "")
+		}, function(err, sd) {
+			if (!err) {
+				sd = loadChildren(sd, docs);
+				res.send(sd);
 			} else {
-				return res.status(500).send(err);
+				if ('CastError' === err.name && 'ObjectId' === err.type) {
+					return res.status(404).send();
+				} else {
+					return res.status(500).send(err);
+				}
 			}
-		}
+		});
 	});
 };
+
+function loadChildren(source, docs) {
+
+	var children = source.children;
+	source.children = [];
+	for (var i = 0; i < children.length; i++) {
+		for (var j = 0; j < docs.length; j++) {
+			// console.log( children[i] + " vs " + docs[j]._id);
+			if (docs[j]._id.equals(children[i])) {
+				var child = loadChildren(docs[j], docs);
+				source.children.push(child);
+				// console.log("found 1");
+			}
+		}
+	}
+	return source;
+}
